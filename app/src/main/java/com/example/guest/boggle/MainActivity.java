831c5c91040922bf4fd6e1769cbd6b5cc3be6ed6
Caleb Stevenson and Shelby Clayton <caleb@>
2016-11-16 15:38:04 -0800

    add joinedLetters to mTextView

--- app/src/main/java/com/example/guest/boggle/MainActivity.java
+++ app/src/main/java/com/example/guest/boggle/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.guest.boggle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
+    @Bind(R.id.textView) TextView mTextView;
+    private static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Random random = new Random();
+
+        String [] consonants = new String[] {"B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"};
+        String [] vowels = new String[] {"A", "E", "I", "O", "U"};
+
+        List<String> selectedLetters = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            int j = random.nextInt(20);
+            selectedLetters.add(consonants[j]);
+        }
+        for (int i = 0; i < 2; i++) {
+            int v = random.nextInt(4);
+            selectedLetters.add(vowels[v]);
+        }
+        String joinedLetters = TextUtils.join(" ", selectedLetters);
+        Log.i(TAG, joinedLetters);
+        mTextView.setText(joinedLetters);
     }
+
 }
