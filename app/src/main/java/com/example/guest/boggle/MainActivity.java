ee1e8b27cd70ae9a185289b66f276833d3980507
Caleb Stevenson and Shelby Clayton <caleb+shelby@>
2016-11-16 16:03:26 -0800

    push submitted words to an ArrayList

--- app/src/main/java/com/example/guest/boggle/MainActivity.java
+++ app/src/main/java/com/example/guest/boggle/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -15,6 +18,9 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.textView) TextView mTextView;
+    @Bind(R.id.submitButton) Button mSubmitButton;
+    @Bind(R.id.editText) EditText mWordEditText;
+    List<String> guessedWords = new ArrayList();
     private static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +30,10 @@ public class MainActivity extends AppCompatActivity {
 
         Random random = new Random();
 
-        String [] consonants = new String[] {"B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"};
+        String [] consonants = new String[] {"B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Qu", "R", "S", "T", "V", "W", "X", "Y", "Z"};
         String [] vowels = new String[] {"A", "E", "I", "O", "U"};
 
-        List<String> selectedLetters = new ArrayList<>();
+        List<String> selectedLetters = new ArrayList();
         for (int i = 0; i < 6; i++) {
             int j = random.nextInt(20);
             selectedLetters.add(consonants[j]);
@@ -39,6 +45,16 @@ public class MainActivity extends AppCompatActivity {
         String joinedLetters = TextUtils.join(" ", selectedLetters);
         Log.i(TAG, joinedLetters);
         mTextView.setText(joinedLetters);
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String word = mWordEditText.getText().toString();
+                guessedWords.add(word);
+            }
+        });
     }
+
+
 
 }
